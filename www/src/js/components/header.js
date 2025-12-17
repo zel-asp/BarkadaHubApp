3783ac64a8d83b262859bc598b17470ecf1fada8
zel-asp <185933672+zel-asp@users.noreply.github.com.>
2025-12-17 16:23:21 +0800

    updated header logo and name

--- www/src/js/components/header.js
+++ www/src/js/components/header.js
@@ -3,37 +3,55 @@ export default function HeaderComponent() {
 <header class="bg-white shadow-sm sticky top-0 z-30 md:mb-10">
     <div class="container mx-auto px-4">
         <div class="flex justify-between items-center py-4">
+
+            <!-- LOGO -->
             <a href="javascript:void(0)" 
-            class="logo flex items-center gap-2 text-black font-extrabold text-xl cursor-pointer"
-            onclick="history.back()"
-            data-page="home">
+                class="logo flex items-center gap-2 font-extrabold text-xl cursor-pointer"
+                onclick="history.back()"
+                data-page="home">
+
                 <img src="../images/image.png" alt="App Logo" class="w-10">
-                <span>BarkadaHub</span>
+
+            <span class="bg-linear-to-r 
+                        from-indigo-600
+                        to-(--color-primary)
+                        bg-clip-text text-transparent">
+                BarkadaHub
+            </span>
+
             </a>
 
+            <!-- USER ACTIONS -->
             <div class="user-actions flex items-center gap-4">
-                <div class="notification-icon relative cursor-pointer">
-                <a href="./notification.html">
-                    <i class="fas fa-bell text-primary text-xl"></i>
-                    <span
-                        class="notification-badge absolute -top-2 -right-1
-                        text-[10px] rounded-full w-4 h-4 flex items-center justify-center font-extrabold text-white bg-red-500">
-                        3
-                    </span>
-                </a>
-                </div>
 
-                <div class="notification-icon relative cursor-pointer">
-                    <a href="./messages.html">
-                        <i class="fa-solid fa-message text-primary text-xl"></i>
+                <!-- NOTIFICATIONS -->
+                <div class="relative cursor-pointer">
+                    <a href="./notification.html" class="block relative">
+                        <i class="fas fa-bell text-primary text-xl"></i>
                         <span
-                            class="notification-badge absolute -top-2 -right-1
-                            text-[10px] rounded-full w-4 h-4 flex items-center justify-center font-extrabold text-white bg-red-500">
+                            class="absolute -top-2 -right-1
+                                text-[10px] rounded-full w-4 h-4
+                                flex items-center justify-center
+                                font-extrabold text-white bg-red-500">
                             3
                         </span>
                     </a>
-                    
                 </div>
+
+                <!-- MESSAGES -->
+                <div class="relative cursor-pointer">
+                    <a href="./messages.html" class="block relative">
+                        <i class="fa-solid fa-message text-primary text-xl"></i>
+                        <span
+                            class="absolute -top-2 -right-1
+                            text-[10px] rounded-full w-4 h-4
+                            flex items-center justify-center
+                            font-extrabold text-white bg-red-500">
+                            3
+                        </span>
+                    </a>
+                </div>
+
             </div>
         </div>
     </div>
