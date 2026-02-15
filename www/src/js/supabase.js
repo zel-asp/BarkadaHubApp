1ed0c271c84d2cd05c95e6dc229fab5703096320
zel-asp <185933672+zel-asp@users.noreply.github.com.>
2026-02-15 16:39:41 +0800

    added js configs

--- www/src/js/supabase.js
+++ www/src/js/supabase.js
@@ -1,1 +1,10 @@
 
+// supabase.js
+import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
+
+const supabaseClient = createClient(
+    "https://mxxrymqwkxxtvxsgkyil.supabase.co",
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im14eHJ5bXF3a3h4dHZ4c2dreWlsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQzMjAzNjgsImV4cCI6MjA3OTg5NjM2OH0.Z2kJCeypENzxwWfwCdO3eRspKgslJswNzi_h8gtdQJE"
+);
+
+export default supabaseClient;
