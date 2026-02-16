a7e7004b5360ead58d9c4cf57dbb2b4a4c61d91a
zel-asp <185933672+zel-asp@users.noreply.github.com.>
2026-02-17 00:17:37 +0800

    Refactor imports and add report management page

--- bundle-entry.js
+++ bundle-entry.js
@@ -1,16 +1,20 @@
 // Root JS
 import './www/src/js/app.js';
-import './www/src/js/functions.js';
 import './www/src/js/supabase.js';
+import './www/src/js/functions.js';
 
 // Render
+import './www/src/js/render/activity.js';
 import './www/src/js/render/Alerts.js';
+import './www/src/js/render/clubs.js';
+import './www/src/js/render/comments.js';
+import './www/src/js/render/message.js';
+import './www/src/js/render/notification.js';
 import './www/src/js/render/offline.js';
 import './www/src/js/render/post.js';
 import './www/src/js/render/profile.js';
 import './www/src/js/render/videos.js';
-import './www/src/js/render/clubs.js';
-import './www/src/js/render/comments.js';
+
 
 // Components
 import './www/src/js/components/header.js';
@@ -22,14 +26,27 @@ import './www/src/js/auth/login.js';
 import './www/src/js/auth/signup.js';
 
 // Pages
+import './www/src/js/pages/activity.js';
 import './www/src/js/pages/clubs.js';
-import './www/src/js/pages/directMesssage.js';
+import './www/src/js/pages/developers.js';
 import './www/src/js/pages/editProfile.js';
 import './www/src/js/pages/home.js';
 import './www/src/js/pages/joinedClub.js';
 import './www/src/js/pages/lost_found.js';
 import './www/src/js/pages/messages.js';
 import './www/src/js/pages/notification.js';
+import './www/src/js/pages/otherProfile.js';
 import './www/src/js/pages/profile.js';
+import './www/src/js/pages/report.js';
 import './www/src/js/pages/videos.js';
-import './www/src/js/pages/developers.js';
+
+//utilities
+import './www/src/js/utils/commentUtils.js';
+import './www/src/js/utils/friendUtils.js';
+import './www/src/js/utils/postDeleteUtils.js';
+import './www/src/js/utils/postUtils.js';
+import './www/src/js/utils/reactionImages.js';
+import './www/src/js/utils/reactionUtils.js';
+import './www/src/js/utils/realtimeReactions.js';
+import './www/src/js/utils/reportUtils.js';
+import './www/src/js/utils/sanitize.js';
